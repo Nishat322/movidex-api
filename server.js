a44e75e2d0b10992846d1f2985c33010578c8db5
Nishat Hoque <hoquen0397@gmail.com>
2021-01-30 18:00:16 -0500

    authorization

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /* eslint-disable indent */
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const data = require('./moviedata');
@@ -7,7 +8,18 @@ const data = require('./moviedata');
 const app = express();
 app.use(morgan('dev'));
 
+app.use(function validateBearerToken(req,res,next){
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization')
+    if (!authToken || authToken.split(' ')[1] !== apiToken){
+        return res.status(401).json({ error: 'Unauthorized request' });
+    }
+    next();
+});
+
 function handleGetMovie (req,res){
+    const {genre, country, avg_vote} = req.query;
+
     res.json(data);
 }
 
