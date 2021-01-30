55a2c30441248095b7e71246404733b81ce9c84d
Nishat Hoque <hoquen0397@gmail.com>
2021-01-30 18:33:28 -0500

    endpoints completed

--- server.js
+++ server.js
@@ -3,14 +3,14 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const data = require('./moviedata');
+const data = require('./moviedata.json');
 
 const app = express();
 app.use(morgan('dev'));
 
 app.use(function validateBearerToken(req,res,next){
     const apiToken = process.env.API_TOKEN;
-    const authToken = req.get('Authorization')
+    const authToken = req.get('Authorization');
     if (!authToken || authToken.split(' ')[1] !== apiToken){
         return res.status(401).json({ error: 'Unauthorized request' });
     }
@@ -19,8 +19,27 @@ app.use(function validateBearerToken(req,res,next){
 
 function handleGetMovie (req,res){
     const {genre, country, avg_vote} = req.query;
+    let filterData = data;
 
-    res.json(data);
+    if(genre){
+        filterData = filterData.filter(movie =>
+            movie.genre.toLowerCase().includes(genre.toLowerCase())
+        );
+    }
+
+    if(country){
+        filterData = filterData.filter(movie => 
+            movie.country.toLowerCase().includes(country.toLowerCase())
+        );
+    }
+
+    if(avg_vote){
+        filterData = filterData.filter(movie => 
+            Number(movie.avg_vote) >= Number(req.query.avg_vote)
+        );
+    }
+
+    res.json(filterData);
 }
 
 app.get('/movie', handleGetMovie);
